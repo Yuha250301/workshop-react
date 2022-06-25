f4ae185b7eca601a4f7aefe7cc90732820fa0fbe
Truong Nguyen Anh Huy <huytnase150173@fpt.edu.vn>
2022-06-25 22:28:48 +0700

    Configuring React

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {Navbar, NavbarBrand} from 'reacstrap';
+import {Navbar, NavbarBrand} from 'reactstrap';
 
 class App extends React.Component {
   render() {
