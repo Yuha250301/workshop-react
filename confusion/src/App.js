a031dc067d2c12917d00a7a883a7edf0b355c77d
Truong Nguyen Anh Huy <huytnase150173@fpt.edu.vn>
2022-07-12 07:31:13 +0700

    Components Part 3_1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from './components/MenuComponent';
 
-import {Navbar, NavbarBrand} from 'reactstrap';
-
-class App extends React.Component {
-  render() {
+function App() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
+        <div className="App">
+            <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                </div>
+            </Navbar>
+            <Menu/>
+        </div>
     );
-  }
 }
 
 export default App;
---
+++ confusion/src/components/Avatar.js
@@ -0,0 +1,7 @@
+function Avatar(props){
+    return(
+        <img src={props.user.avatarUrl} alt={props.user.name} />
+    );
+}
+
+export default Avatar;
