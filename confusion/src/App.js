484ebcddbe478420d5d3a727c5cee053a0441ddb
Truong Nguyen Anh Huy <huytnase150173@fpt.edu.vn>
2022-07-12 07:57:59 +0700

    Complete Part 3_2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,8 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from './components/MenuComponent';
+import { useState } from 'react';
+import { DISHES } from './shared/dish2.js';
 
 function App() {
+    const [dishes, setDish] = useState(DISHES);
+
     return (
         <div className="App">
             <Navbar dark color="primary">
@@ -11,7 +15,7 @@ function App() {
                     <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                 </div>
             </Navbar>
-            <Menu/>
+            <Menu dishes={dishes}/>
         </div>
     );
 }
