e68d23f43ed8d6868ba21dbefa52a29367ac3ff8
Truong Nguyen Anh Huy <huytnase150173@fpt.edu.vn>
2022-07-12 10:58:06 +0700

    Presentational and Container Components

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -4,18 +4,14 @@ import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from './components/MenuComponent';
 import { useState } from 'react';
 import { DISHES } from './shared/dish.js';
+import Main from "./components/MainComponent";
 
 function App() {
     const [dishes, setDish] = useState(DISHES);
 
     return (
         <div className="App">
-            <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                </div>
-            </Navbar>
-            <Menu dishes={dishes}/>
+            <Main/>
         </div>
     );
 }
