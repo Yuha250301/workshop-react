bb9ccab43651d847054f17e7715a66dbd15d6e06
Truong Nguyen Anh Huy <huytnase150173@fpt.edu.vn>
2022-07-19 07:52:12 +0700

    Done all workshop

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -4,17 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import { ConfigureStore } from "./redux/configureStore";
 import { Provider } from "react-redux";
 
+import TestFetchComponent from "./use-fetch/TestFetchComponent"
+
 const store = ConfigureStore();
 
 function App() {
     return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <div className="App">
-                    <Main />
-                </div>
-            </BrowserRouter>
-        </Provider>
+        // <Provider store={store}>
+        //     <BrowserRouter>
+        //         <div className="App">
+        //             <Main />
+        //         </div>
+        //     </BrowserRouter>
+        // </Provider>
+        <TestFetchComponent/>
     );
 }
 
